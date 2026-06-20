3ea161db81b45fe1d3e6441149a7c5c02d6b493e
mauliksumaniya-wq <mauliksumaniya@gmail.com>
2026-06-20 22:50:15 +0530

    Refactor service worker registration logic

--- sw.js
+++ sw.js
@@ -1,7 +1,4 @@
-self.addEventListener('install', (e) => {
-  console.log('NamasteOtaku SW Installed');
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
-});
+if ('serviceWorker' in navigator) {
+  
+  navigator.serviceWorker.register('sw.js?v=2');
+}
